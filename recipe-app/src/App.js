fdd10892d0219fffc39143076b6cb85ec39c8e3a
DeanRimmer <dean.rimmer1313@gmail.com>
2022-06-30 20:35:37 +0100

    Collections banner and image in place

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Components/Header.js";
+import Collections from "./Components/Collections.js";
 import React from "react";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Collections />
     </div>
   );
 }
---
+++ recipe-app/src/Components/Collections.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "../App.css";
+
+class Collections extends React.Component {
+  render() {
+    return (
+      <div className="CollectionsContainer">
+        <div className="Collection">
+          <div className="TextContainer">
+            <a href="#">cat</a>
+          </div>
+        </div>
+        <div className="Collection">
+          <div className="TextContainer">
+            <a href="#">cat</a>
+          </div>
+        </div>
+        <div className="Collection">
+          <div className="TextContainer">
+            <a href="#">cat</a>
+          </div>
+        </div>
+        <div className="Collection">
+          <div className="TextContainer">
+            <a href="#">cat</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Collections;
